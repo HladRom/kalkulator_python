968f9d710b62e364105295d34512f057bcb100cf
HladRom <email@example.com>
2023-08-06 10:08:23 +0200

    multiplication table

---
+++ test3.py
@@ -0,0 +1,20 @@
+print ("multiplication tebe")
+text = int(input("enter a number: "))
+A = text * 1
+print(A)
+A = text * 2
+print(A)
+A = text * 3
+print(A)
+A = text * 4
+print(A)
+A = text * 5
+print(A)
+A = text * 6
+print(A)
+A = text * 7
+print(A)
+A = text * 8
+print(A)
+A = text * 9
+print ("Your multiplication table is ready! ")
