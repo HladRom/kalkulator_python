7d76f3c17d64dfd0aade18aef6a522c6f766e7df
HladRom <email@example.com>
2023-08-13 16:38:22 +0200

    test program9

--- test.py
+++ test.py
@@ -13,6 +13,31 @@
 #    sum += number
 #    print ('Сума чисел від 1 до 100', sum)
 #----------------------------------------------------
-for numb in range (1,6):
-    S = numb * numb
-    print('квадрат',numb ,'дорівнбює',S)
+#for numb in range (1,6):
+#    S = numb * numb
+#    print('квадрат',numb ,'дорівнбює',S)
+#----------------------------------------------------
+#def add_numbers():
+#    num1 = float (input('Ввкдіть число: '))
+#    num2 = float (input('Ввкдіть число: '))
+#    sum = num1 + num2
+#    print ('Сума чисел:',sum)
+#add_numbers()
+#----------------------------------------------------
+#def string_length():
+#    user_input = input('Введіть число:')
+#    lenght = len (user_input)
+#    return lenght
+#string_length = string_length()
+#print('Довжина рядка: ',string_length)
+#----------------------------------------------------
+def is_even (number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+user_number = int(input('Введіть число: '))
+if is_even(user_number):
+    print('Число є парним: ')
+else:
+    print('Число є не парним: ')
