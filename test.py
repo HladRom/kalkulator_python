979c0f1535cb5d2ba9b3cf080db1077f3227d87c
HladRom <email@example.com>
2023-08-13 15:03:43 +0200

    test program

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-n = int(input('Add number:'))
-sum = 0 
+#n = int(input('Add number:'))
+#sum = 0 
 
-for i in range (1, n+1):
-    sum += 1
-print('sum yours numbers', n, 'natural numbers',sum)
+#for i in range (1, n+1):
+#    sum += 1
+#print('sum yours numbers', n, 'natural numbers',sum)
+#----------------------------------------------------
+#for number in range (1,11):
+#    print(number)
+#----------------------------------------------------
+#sum = 0 
+#for number in range (1,101):
+#    sum += number
+#    print ('Сума чисел від 1 до 100', sum)
+#----------------------------------------------------
+for numb in range (1,6):
+    S = numb * numb
+    print('квадрат',numb ,'дорівнбює',S)
