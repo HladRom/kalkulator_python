08e2c098ec9ff7065f3ca0ba487b5a9216832c0e
HladRom <114509954+HladRom@users.noreply.github.com>
2024-03-01 21:42:27 +0100

    update code(firefox.py)
     subprocess.run(['firefox'])
    
    Co-Authored-By: HladRom <114509954+hladrom@users.noreply.github.com>

--- firefox.py
+++ firefox.py
@@ -1,9 +1,10 @@
 #python
+
 import subprocess
 
 def open_firefox():
     try:
-        subprocess.run(['firefox'])
+        subprocess.run(['firefox']) #This line attempts to run the Firefox application using the subprocess.run() function. It passes a list containing the command to run Firefox.
     except FileNotFoundError:
         print("Будь ласка, встановіть Firefox на своїй системі!")
 
